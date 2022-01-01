2545597d43efac0e63f584144c9b73d2c5103f8f
ugrsen <93542523+ugrsen@users.noreply.github.com>
2022-01-02 03:33:32 +0530

    Add files via upload
    commit

---
+++ index1.js
@@ -0,0 +1,17 @@
+//Check whether the given chracter is a vowel or not. (Also check for capital )
+var char = prompt("Enter any character");
+
+
+if((char == "a") || (char == "A")) {
+ alert("It is a vowel");
+} else if((char == "e") || (char == "E")) {
+ alert("It is a vowel");
+} else if((char == "i") || (char == "I")) {
+ alert("It is a vowel");
+} else if((char == "o") || (char == "O")) {
+ alert("It is a vowel");
+} else if((char == "u") || (char == "U")) {
+ alert("It is a vowel");
+} else {
+ alert("It is a consonant");
+}
